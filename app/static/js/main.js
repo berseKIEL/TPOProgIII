bbf3358bb56e82477261713555d780e975682a2f
BerseKIEL <exebarco280320@gmail.com>
2022-10-29 10:15:45 -0300

    habilidades completas

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -31,3 +31,52 @@ function clickLink() {
 }
 
 navLink.forEach(n => n.addEventListener('click', clickLink))
+
+// Habilidades
+// Se selecciona un data target en caso de que clickee un determinado tab,
+// Eso permite abrir un determinado contenido teniendo en cuenta cual clickee
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
+
+// Por cada una de los tabs..
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('habilidades-activo');
+        })
+
+        target.classList.add('habilidades-activo');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('habilidades-activo')
+        })
+
+        tab.classList.add('habilidades-activo')
+
+    })
+})
+
+// Click en Flecha para abrir image
+const ContHabi = document.getElementsByClassName('habilidades-contenido-dos');
+const Header = document.querySelectorAll('.habilidades-habilidad-dos-header')
+
+function toggleHabilidad() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < ContHabi.length; i++) {
+        ContHabi[i].className = 'habilidades-contenido-dos habilidades-cerradas';
+    }
+
+    if(itemClass == 'habilidades-contenido-dos habilidades-cerradas') {
+        this.parentNode.className = 'habilidades-contenido-dos habilidades-abiertas';
+    }
+    else {
+        this.parentNode.className = 'habilidades-contenido-dos habilidades-cerradas';
+    }
+}
+
+Header.forEach((el) => {
+    el.addEventListener('click', toggleHabilidad)
+})
